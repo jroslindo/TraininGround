c6350835f392df28af356380865f043eddf8197e
jroslindo <jroslindotab@gmail.com>
2025-08-15 09:48:19 -0300

    Movement animation

---
+++ Coliseu/Source/Coliseu/Private/Characters/SlashAnimInstance.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/SlashAnimInstance.h"
+#include "Characters/SlashCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "kismet/kismetMathLibrary.h"
+
+void USlashAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeUninitializeAnimation();
+
+	this->SlashCharacter = Cast<ASlashCharacter>(this->TryGetPawnOwner());
+
+	if (this->SlashCharacter)
+	{
+		this->SlashCharacterMovement = this->SlashCharacter->GetCharacterMovement();
+	}
+	 
+}
+
+void USlashAnimInstance::NativeUpdateAnimation(float DeltaTime)
+{
+	Super::NativeUpdateAnimation(DeltaTime);
+
+	if (this->SlashCharacterMovement)
+	{
+		this->GroundSpeed = UKismetMathLibrary::VSizeXY(this->SlashCharacterMovement->Velocity);
+	}
+}
